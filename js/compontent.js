36a7e91d2f973318cbeeedd873003a03322aea29
margolee <margo.lee@hyweb.com.tw>
2020-12-08 09:53:56 +0800

    整理js路徑

--- js/compontent.js
+++ js/compontent.js
@@ -12,27 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
 $(document).ready(function () {
   $(".tabs").tabs();
 });
-// let tab = document.querySelectorAll(".c-tabs__item__link");
-// let content = document.querySelectorAll(".content");
-// let clickTab;
-// for (let i = 0; i < tab.length; i++) {
-//   tab[i].addEventListener("click", function () {
-//     clickTab = tab[i].getAttribute("data-num");
-//     compare();
-//   });
-// }
-// function compare() {
-//   for (let a = 0; a < content.length; a++) {
-//     let clickContent = content[a].getAttribute("data-content");
-//     if (clickTab == clickContent) {
-//       content[a].classList.add("is-show");
-//       tab[a].classList.add("is-active");
-//     } else {
-//       content[a].classList.remove("is-show");
-//       tab[a].classList.remove("is-active");
-//     }
-//   }
-// }
 
 //++++++++++++++++++
 //  select效果
@@ -60,14 +39,16 @@ document.addEventListener("DOMContentLoaded", function () {
 //++++++++++++++++++
 //  星星樣式改變
 //++++++++++++++++++
-let starBtn = document.querySelectorAll(".o-card__star");
+let starBtn = document.querySelectorAll(".o-card__iconBtn");
 if (starBtn != null) {
   for (let i = 0; i < starBtn.length; i++) {
     starBtn[i].addEventListener("click", function () {
-      if (starBtn[i].classList.value == "o-card__star o-card__star-active") {
-        starBtn[i].classList.value = "o-card__star o-card__star-default";
+      if (starBtn[i].classList.contains("o-card__star-active") === true) {
+        starBtn[i].classList.remove("o-card__star-active");
+        starBtn[i].classList.add("o-card__star-default");
       } else {
-        starBtn[i].classList.value = "o-card__star o-card__star-active";
+        starBtn[i].classList.remove("o-card__star-default");
+        starBtn[i].classList.add("o-card__star-active");
       }
     });
   }
@@ -173,21 +154,6 @@ if ($("#SingleDatePicker") != null) {
     }
   );
 }
-//++++++++++++++++++
-//dropdown-input
-//++++++++++++++++++
-let dropdown = document.querySelectorAll(".dropdown");
-let dropdownInput = document.querySelectorAll(".dropdown-input");
-//let dropdownOverlay = document.querySelectorAll(".dropdown-overlay");
-// for (let a = 0; a < dropdownInput.length; a++) {
-//   //console.log(dropdownInput[a]);
-//   dropdownInput[a].addEventListener("click", function () {
-//     dropdown[a].classList.add("is-dropdown-open");
-//     // let lay = document.getElementById("body");
-//     // lay.classList.add("dropdown-overlay");
-//     document.body.classList.add("dropdown-overlay");
-//   });
-// }
 
 //++++++++++++++++++
 //popover 效果
@@ -225,7 +191,6 @@ for (let a = 0; a < pop_modal.length; a++) {
 //++++++++++++++++++
 //點擊 accordion-header 底圖顏色變色
 const collapsible = document.querySelectorAll(".collapsible");
-
 collapsible.forEach(function (item) {
   item.addEventListener("click", function () {
     let a = item.classList.contains("is-accordion-active");
