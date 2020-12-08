e7bbc727706d2e222010125b14b5728564deb970
margolee <margo.lee@hyweb.com.tw>
2020-12-08 16:55:58 +0800

    新增_page_15.a.0

--- js/page_function.js
+++ js/page_function.js
@@ -2,20 +2,37 @@
 //  更換卡片及列表樣式
 //++++++++++++++++++++++
 
-const o_list = document.querySelector(".o-list");
-const o_card = document.querySelector(".o-card");
-const l_main = document.querySelector(".l-main ");
-if (o_list != null) {
-  o_list.addEventListener("click", function () {
-    l_main.classList.add("change-card-to-list");
-    o_list.style.display = "none";
-    o_card.style.display = "inline";
-  });
+const o_list = document.querySelectorAll(".o-list");
+const o_card = document.querySelectorAll(".o-card");
+const l_main = document.querySelectorAll(".l-main ");
+for (let r = 0; r < l_main.length; r++) {
+  if (o_list[r] != null) {
+    o_list[r].addEventListener("click", function () {
+      l_main[r].classList.add("change-card-to-list");
+      o_list[r].style.display = "none";
+      o_card[r].style.display = "inline";
+    });
+  }
+  if (o_card[r] != null) {
+    o_card[r].addEventListener("click", function () {
+      l_main[r].classList.remove("change-card-to-list");
+      o_card[r].style.display = "none";
+      o_list[r].style.display = "inline";
+    });
+  }
 }
-if (o_card != null) {
-  o_card.addEventListener("click", function () {
-    l_main.classList.remove("change-card-to-list");
-    o_card.style.display = "none";
-    o_list.style.display = "inline";
-  });
-}
+
+// if (o_list != null) {
+//   o_list.addEventListener("click", function () {
+//     l_main.classList.add("change-card-to-list");
+//     o_list.style.display = "none";
+//     o_card.style.display = "inline";
+//   });
+// }
+// if (o_card != null) {
+//   o_card.addEventListener("click", function () {
+//     l_main.classList.remove("change-card-to-list");
+//     o_card.style.display = "none";
+//     o_list.style.display = "inline";
+//   });
+// }
